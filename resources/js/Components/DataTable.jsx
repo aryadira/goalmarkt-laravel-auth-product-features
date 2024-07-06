b9aa33239df65a0e41eaffa5ff7c4aafedb28f23
aryadira <aryadyra27@gmail.com>
2024-07-06 21:45:18 +0700

    add table testing

--- resources/js/Components/DataTable.jsx
+++ resources/js/Components/DataTable.jsx
@@ -1,36 +1,36 @@
 import * as React from "react";
-import { DataGrid } from "@mui/x-data-grid";
+import { DataGrid, renderActionsCell } from "@mui/x-data-grid";
+import { Button } from "@mui/material";
 
 const columns = [
     { field: "id", headerName: "ID" },
     { field: "product_name", headerName: "Product Info" },
+    { field: "price", headerName: "Price" },
+    { field: "expiry_date", headerName: "Expiry Date", sortable: false },
     {
-        field: "price",
-        headerName: "Price",
-        type: "number",
-    },
-    {
-        field: "expiry_date",
-        headerName: "Expiry Date",
-        sortable: false,
+        headerName: "Actions",
+        renderCell: () => {
+            return <Button variant="contained">Action</Button>;
+        },
     },
 ];
 
 export default function DataTable({ data }) {
     return (
-        <div style={{ height: 400, width: "100%" }}>
+        <div className="h-full max-h-max">
             <DataGrid
                 rows={data}
                 columns={columns}
                 initialState={{
                     pagination: {
-                        paginationModel: { page: 0, pageSize: 5 },
+                        paginationModel: { page: 0, pageSize: 10 },
                     },
                 }}
-                pageSizeOptions={[5, 10]}
+                pageSizeOptions={[10, 20]}
+                autoHeight={true}
+                autosizing
                 checkboxSelection
             />
-            
         </div>
     );
 }
